0f6e2ec35ca8b262ddec28ced04d02f27569b1e0
Matt Suhay <matthew.suhay@gmail.com>
2020-08-16 18:49:25 -0500

    Security patching

--- src/index.js
+++ src/index.js
@@ -43,22 +43,6 @@ const server = new ApolloServer({
   },
 })
 
-if (process.env.NODE_ENV === 'development') {
-  (() => {
-    const webpack = require('webpack')
-    const webpackConfig = require(process.env.WEBPACK_CONFIG ? process.env.WEBPACK_CONFIG : '../webpack.config')
-    const compiler = webpack(webpackConfig)
-
-    app.use(require('webpack-dev-middleware')(compiler, {
-      logLevel: 'warn', publicPath: webpackConfig.output.publicPath,
-    }))
-
-    app.use(require('webpack-hot-middleware')(compiler, {
-      log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000,
-    }))
-  })()
-}
-
 app.use(express.static(path.resolve('./public')))
 
 app.all('/', (req, res) => {
